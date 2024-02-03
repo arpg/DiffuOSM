039391b1779cc772b640a04aa11020ffec831afe
Doncey Albin <donceyalbin@gmail.com>
2024-02-03 13:47:16 -0700

    Added main file, but may remove ...

---
+++ OSM_merge/main.py
@@ -0,0 +1,29 @@
+"""
+Extract training data for Diffusion.
+
+By: Doncey Albin
+
+"""
+
+
+# 1) Create vel poses txt for all sequences
+
+# 2) Create all data_oxsx
+
+# 3) Get labels for each frame in each seq
+
+# 4) Get osm for each seq
+
+# 5) Get extracted building points for each building seq
+
+def extract_and_save_building_points():
+    seq = "0005"
+
+def main(): 
+    extract_and_save_building_points()
+  
+  
+# Using the special variable  
+# __name__ 
+if __name__=="__main__": 
+    main() 
