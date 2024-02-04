068538fa5be364cdc9f798833a04bc5a68f2e26d
Doncey Albin <donceyalbin@gmail.com>
2024-02-03 20:34:55 -0700

    init dir & code

---
+++ OSM_merge/helpers/trans_pose_data.py
@@ -0,0 +1,44 @@
+'''
+Doncey Albin
+
+
+'''
+
+import os
+import open3d as o3d
+from open3d.visualization import gui
+import numpy as np
+from collections import namedtuple
+import osmnx as ox
+
+# Internal
+from tools.labels import labels
+from tools.utils import *
+from tools.convert_oxts_pose import *
+
+# Define the translation vector from IMU to LiDAR
+translation_vector = np.array([0.81, 0.32, -0.83])
+
+# Define the rotation matrix for a 180-degree rotation about the X-axis (imu -> lidar)
+rotation_matrix = np.array([[1, 0, 0],
+                            [0, -1, 0],
+                            [0, 0, -1]])
+
+imu_poses_file = 'kitti360Scripts/data/KITTI360/data_poses/2013_05_28_drive_0005_sync/poses.txt'
+vel_poses_file = 'kitti360Scripts/data/KITTI360/data_poses/2013_05_28_drive_0005_sync/vel_poses.txt'
+
+# Read the IMU poses
+transformation_matrices = read_poses(imu_poses_file)
+
+# Extract frame indices (assuming they are the first element in each line)
+frame_indices = []
+with open(imu_poses_file, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        frame_indices.append(line.split()[0])
+
+# Transform IMU poses to LiDAR poses
+lidar_poses = transform_imu_to_lidar(transformation_matrices, translation_vector, rotation_matrix)
+
+# Write the LiDAR poses to file
+write_poses(vel_poses_file, lidar_poses, frame_indices)
