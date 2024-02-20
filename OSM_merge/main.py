e8393963eec1c1c76a29518a127e2be1121e9938
Doncey Albin <donceyalbin@gmail.com>
2024-02-20 14:06:35 -0700

    adjusted frame inc to 100

--- OSM_merge/main.py
+++ OSM_merge/main.py
@@ -1,8 +1,11 @@
 """
 By: Doncey Albin
 
-- Use building points to get hit_building_list & Only use buildings that have been hit on more than 3 times
-- Use only edges that accum points lie on for each building
+- Save building edges as a pickle file (or bin) and do checking if it exist.
+- Use building points to get hit_building_list & Only use buildings that have been hit on more than 2 edges
+- Do batches of point accumulations
+
+
 - Remove interior edges from buildings
 
 """
@@ -14,13 +17,19 @@ from datetime import datetime
 
 def main():
     seq_list = [0, 2, 3, 4, 5, 6, 7, 9, 10]
-    frame_inc = 10
+    frame_inc = 100
     for seq in seq_list:
-        extractBuildingData(seq, frame_inc)
         curr_time = datetime.now()
         curr_time_str = curr_time.strftime('%Y-%m-%d %H:%M:%S')
         with open('/home/donceykong/kitti_360/kitti360Scripts/completed.txt', 'a') as file:
-            file.write(f'\nSequence {seq} completed. Timestamp: {curr_time_str}')
+            file.write(f'\nSequence {seq} started. Timestamp: {curr_time_str}/n')
+
+        extractBuildingData(seq, frame_inc)
+
+        curr_time = datetime.now()
+        curr_time_str = curr_time.strftime('%Y-%m-%d %H:%M:%S')
+        with open('/home/donceykong/kitti_360/kitti360Scripts/completed.txt', 'a') as file:
+            file.write(f'\nSequence {seq} completed. Timestamp: {curr_time_str}/n')
 
     # # Just for testing
     # extractBuildingData(2, frame_inc)
