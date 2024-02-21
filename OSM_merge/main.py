4f65aeb62f0923c6dc81a15cd975a5a24539599a
Doncey Albin <donceyalbin@gmail.com>
2024-02-20 17:23:17 -0700

    Fixed file writing new line

--- OSM_merge/main.py
+++ OSM_merge/main.py
@@ -16,20 +16,22 @@ from extract_building_data import extractBuildingData
 from datetime import datetime
 
 def main():
+    monitor_file = './completed.txt'
     seq_list = [0, 2, 3, 4, 5, 6, 7, 9, 10]
-    frame_inc = 100
+    frame_inc = 1
     for seq in seq_list:
         curr_time = datetime.now()
         curr_time_str = curr_time.strftime('%Y-%m-%d %H:%M:%S')
-        with open('/home/donceykong/kitti_360/kitti360Scripts/completed.txt', 'a') as file:
-            file.write(f'\nSequence {seq} started. Timestamp: {curr_time_str}/n')
+        with open(monitor_file, 'a') as file:
+            file.write(f'\nSequence {seq} started. Timestamp: {curr_time_str}')
 
-        extractBuildingData(seq, frame_inc)
+        # extractBuildingData(seq, frame_inc)
 
         curr_time = datetime.now()
         curr_time_str = curr_time.strftime('%Y-%m-%d %H:%M:%S')
-        with open('/home/donceykong/kitti_360/kitti360Scripts/completed.txt', 'a') as file:
-            file.write(f'\nSequence {seq} completed. Timestamp: {curr_time_str}/n')
+        with open(monitor_file, 'a') as file:
+            file.write(f'\nSequence {seq} completed. Timestamp: {curr_time_str}\n')
+            file.write(' ')
 
     # # Just for testing
     # extractBuildingData(2, frame_inc)
