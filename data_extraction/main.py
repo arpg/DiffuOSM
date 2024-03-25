876c8c34b82235e608b40d29bf48068d9ff2cb1e
Doncey Albin <donceyalbin@gmail.com>
2024-03-25 05:38:20 -0600

    Removed old argument from class call

--- data_extraction/main.py
+++ data_extraction/main.py
@@ -8,11 +8,10 @@ import os
 from tools.extract_building_data import extractBuildingData
 
 def main():
-    monitor_file = './extracted_all.txt'
     seq_list = [0, 2, 3, 4, 5, 6, 7, 9, 10]
     frame_inc = 1
     for seq in seq_list:
-        extractBuildingData(seq, frame_inc, monitor_file)
+        extractBuildingData(seq, frame_inc)
 
 if __name__=="__main__": 
     main() 
