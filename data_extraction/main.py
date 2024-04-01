c63b3233f080d22e4ac864bb85650377a54db3c7
Doncey Albin <donceyalbin@gmail.com>
2024-04-01 01:59:43 -0600

    Updated which module & class main calls for data extraction

--- data_extraction/main.py
+++ data_extraction/main.py
@@ -5,13 +5,18 @@ By: Doncey Albin
 
 import argparse
 import os
-from tools.extract_building_data import extractBuildingData
+from tools.extract_build_data import ExtractBuildingData
+
+def test_pass_by_ref(this_list):
+    new_list = (1, 2)
+    this_list.extend(new_list)
 
 def main():
-    seq_list = [0, 2, 3, 4, 5, 6, 7, 9, 10]
+    # seq_list = [0, 2, 3, 4, 5, 6, 7, 9, 10]
+    seq_list = [0]
     frame_inc = 1
     for seq in seq_list:
-        extractBuildingData(seq, frame_inc)
+        ExtractBuildingData(seq, frame_inc)
 
 if __name__=="__main__": 
     main() 
