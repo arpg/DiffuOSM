9997b06874a076488a8637c6e06b5220efec17c5
Doncey Albin <donceyalbin@gmail.com>
2024-04-03 03:23:16 -0600

    Small adjustments here

--- data_extraction/main.py
+++ data_extraction/main.py
@@ -7,13 +7,8 @@ import argparse
 import os
 from tools.extract_build_data import ExtractBuildingData
 
-def test_pass_by_ref(this_list):
-    new_list = (1, 2)
-    this_list.extend(new_list)
-
 def main():
-    # seq_list = [0, 2, 3, 4, 5, 6, 7, 9, 10]
-    seq_list = [0]
+    seq_list = [0, 2, 3, 4, 5, 6, 7, 9, 10]
     frame_inc = 1
     for seq in seq_list:
         ExtractBuildingData(seq, frame_inc)
