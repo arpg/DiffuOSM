c2f98814571ce7cba571306670b80c86316021ab
Doncey Albin <donceyalbin@gmail.com>
2024-04-10 19:07:16 -0600

    Using class methods outside

--- data_extraction/main.py
+++ data_extraction/main.py
@@ -8,7 +8,7 @@ import os
 from tools.extract_build_data import ExtractBuildingData
 
 def main():
-    seq_list = [0, 2, 3, 4, 5, 6, 7, 9, 10]
+    seq_list = [0] #, 2, 3, 4, 5, 6, 7, 9, 10]
     frame_inc = 1
     for seq in seq_list:
         seq_extract = ExtractBuildingData(seq, frame_inc)
@@ -20,6 +20,7 @@ def main():
         seq_extract.initiate_extraction()
         seq_extract = ExtractBuildingData(seq, frame_inc)
         seq_extract.save_all_obs_points()
+        seq_extract.extract_obs_and_accum_obs_points()
         seq_extract.remove_saved_build_dicts()
         seq_extract.conclude_extraction()
 
