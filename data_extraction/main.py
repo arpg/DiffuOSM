87295f33fe16c76486b21fb2a5629a28c800b54c
Doncey Albin <donceyalbin@gmail.com>
2024-04-10 19:10:33 -0600

    Using class methods outside

--- data_extraction/main.py
+++ data_extraction/main.py
@@ -17,8 +17,8 @@ def main():
         seq_extract.conclude_extraction()
 
     for seq in seq_list:
-        seq_extract.initiate_extraction()
         seq_extract = ExtractBuildingData(seq, frame_inc)
+        seq_extract.initiate_extraction()
         seq_extract.extract_obs_and_accum_obs_points()
         seq_extract.save_all_obs_points()
         seq_extract.remove_saved_build_dicts()
