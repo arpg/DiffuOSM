7506afe536bd55e186794c62561aeec4e3b9732b
Doncey Albin <donceyalbin@gmail.com>
2024-04-19 16:37:29 -0600

    Checking len of curr observed points frame

--- data_extraction/tools/extract_build_data.py
+++ data_extraction/tools/extract_build_data.py
@@ -312,12 +312,12 @@ class ExtractBuildingData:
 
             # Center DS frame about robot lidar
             pos_latlong = self.velodyne_poses_latlon.get(frame_num)[:3]
-            print(f"\nbuild_edges array[0]: {np.asarray(building_edges_frame)[0]}")
+            print(f"\nbuild_edges array[0][0]: {np.asarray(building_edges_frame)[0][0]}")
             building_edges_frame = np.asarray(building_edges_frame) - pos_latlong
-            print("unobs")
+
             unobserved_curr_accum_points_frame = np.asarray(unobserved_curr_accum_points_frame) - pos_latlong
-            print("curr")
-            print(f"len_curr: {len(np.asarray(curr_accum_points_frame))}")
+            print(f"unobserved_curr_accum_points_frame: {unobserved_curr_accum_points_frame}")
+
             curr_accum_points_frame = np.asarray(curr_accum_points_frame) - pos_latlong
             # Test the mean of the points in this frame
             # print(f"Mean lat: {np.mean(curr_accum_points_frame[:,0])}, Mean lon: {np.mean(curr_accum_points_frame[:,1])}")
